4136746bd9db179dc01712ff7748afcecbf11f3a
Henrik Tronstad <Henrik.Tronstad@dfo.no>
2024-04-15 13:07:12 +0200

    added pre-commit hook again

---
+++ test.ts
@@ -0,0 +1,18 @@
+const another: string =   
+  'hey';
+
+const anotherFunction = (another: string): string => {
+    console.log(another
+
+    );
+    
+  return another;
+}
+
+const obj = {
+  another: 'hey',
+  anotherFunction: (another: string): string => {
+    console.log(another);
+    return another;
+  },
+}
