afba700a5f6eb6b32ec41afd1b41bca36776388b
Henrik Tronstad <Henrik.Tronstad@dfo.no>
2024-04-17 09:00:41 +0200

    using prettier in vs code

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,7 @@
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Still not formatted" />
+      <HelloWorld msg="Should be formatted" />
     </div>
   </header>
 
